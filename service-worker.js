bc1c845b0f2b494f38d3327f3a972dd66fe67cc5
RSA1969 <bubshkin@yahoo.com>
2026-03-06 11:06:24 +0100

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE = "kris-halmstad-v2"; 
+const ASSETS = ["/", "/index.html", "/manifest.webmanifest", "/service-worker.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
